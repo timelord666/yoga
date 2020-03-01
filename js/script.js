905f5ef8624e0dc86c3a14d5a6919d5d447cb4d5
timelord <timemonster666@gmail.com>
2020-03-01 23:42:06 +0500

    Add project files

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $('.awaits__slider').slick({
+    accessibility: false,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+
+
+
+  });
+  $('.program__slider').slick({
+    accessibility: false,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+
+
+
+  });
+  $('.review__slider').slick({
+
+    autoplay: true,
+    dots: true,
+    arrows: false
+
+ 
+
+
+
+
+  });
+
+
+
+});
